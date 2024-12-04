8ed681e88ce862d1205356983fa1cb6f11fc1e92
oscar jossue hernadez rodriguez <osscarodriguez@gmail.com>
2024-12-03 20:11:49 -0600

    nuevos cambios con las variables ahora las coloque mas parecido al video tutorial

--- build/keys.js
+++ build/keys.js
@@ -1,13 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+/* import dotenv from 'dotenv'; */
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
 // Cargar variables de entorno solo en desarrollo
-if (process.env.NODE_ENV !== "production") {
-    dotenv_1.default.config(); // Carga el archivo .env solo si no estamos en producción
-}
+/* if (process.env.NODE_ENV !== "production") {
+    dotenv.config();  // Carga el archivo .env solo si no estamos en producción
+} */
 /* export default {
     database: {
         host: process.env.DB_HOST,   // Asegúrate de que las variables estén bien definidas en el archivo .env
@@ -18,12 +15,23 @@ if (process.env.NODE_ENV !== "production") {
     }
 };
  */
-exports.default = {
+/* export default {
     database: {
-        host: "mysql.railway.internal",
+        host: "mysql.railway.internal",   // Asegúrate de que las variables estén bien definidas en el archivo .env
         user: "root",
         password: "sdxnPWePqOMxPqNQXLsnYUjKdwOKAWxG",
         database: "railway",
         port: 3306,
+    }
+};
+ */
+const config_1 = require("./config");
+exports.default = {
+    database: {
+        host: config_1.DB_HOST,
+        user: config_1.DB_USER,
+        password: config_1.DB_PASSWORD,
+        database: config_1.DB_NAME,
+        port: config_1.DB_PORT,
     }
 };
